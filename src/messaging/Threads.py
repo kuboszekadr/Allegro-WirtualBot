59947ccc9b13db4fcab743df82746d575b664ee5
Adrian Kuboszek <kuboszekadr@gmail.com>
2023-12-07 09:47:25 +0100

    Bug fixes

--- src/messaging/Threads.py
+++ src/messaging/Threads.py
@@ -6,13 +6,13 @@ import logging
 class Threads:
     endpoint = 'https://api.allegro.pl/messaging/threads'
 
-    def __init__(self, device_token: str) -> None:
-        self.device_token = device_token
+    def __init__(self, access_token: str) -> None:
+        self.access_token = access_token
 
     @property
     def _headers(self):
         headers = {
-            'Authorization': f'Bearer {self.device_token}',
+            'Authorization': f'Bearer {self.access_token}',
             'Accept': 'application/vnd.allegro.public.v1+json',
             'Content-Type': 'application/vnd.allegro.public.v1+json'
         }    
@@ -40,7 +40,6 @@ class Threads:
 
         logging.info("Message posted successfully.")
 
-
     @property
     def thread_list(self, limit: int = 5) -> dict:
         r = requests.get(
@@ -57,3 +56,33 @@ class Threads:
 
         results = r.json()['threads']
         return results
+
+    def get(self, thread_id: str) -> dict:
+        r = requests.get(
+            f"{self.endpoint}/{thread_id}",
+            headers=self._headers,
+        )
+        result = r.json()
+        return result
+
+if __name__ == '__main__':
+    import os
+    import sys
+
+    sys.path.append(os.getcwd())
+    
+    from src.auth.OAuth import OAuth
+    from dotenv import load_dotenv
+
+    load_dotenv()
+    oauth = OAuth(
+        client_id = os.environ['CLIENT_ID'],
+        client_secret= os.environ['CLIENT_SECRET']
+    )
+    token = oauth.token[0]
+
+    threads = Threads(access_token=token)
+
+    threads_list = threads.thread_list
+    thread = threads.get(threads_list[0]['id'])
+    pass
