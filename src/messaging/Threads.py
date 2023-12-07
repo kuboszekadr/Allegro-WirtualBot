2fdbed4cd5f93111c74e18b5e6658e972538bc8d
Dwd <dwd@Dwds-MacBook-Pro.local>
2023-12-07 14:51:13 +0100

    k

--- src/messaging/Threads.py
+++ src/messaging/Threads.py
@@ -71,14 +71,14 @@ class Threads:
         result = r.json()
         return result
 
-    def list_messages(self, thread_id: str, limit: int=None, offset: int=None, before: str=None, after: str=None):
+    def list_messages(self, thread_id: str, limit: int = None, offset: int = None, before: str = None, after: str = None):
         params = {
             'limit': limit,
             'offset': offset,
             'before': before,
             'after': after
         }
-        params = {k:v for k, v in params.items() if v}
+        params = {k: v for k, v in params.items() if v}
 
         r = requests.get(
             f"{self.endpoint}/{thread_id}/messages",
@@ -95,7 +95,8 @@ class Threads:
             reverse=True
         )
 
-        msgs_filtered = filter(lambda x: x['author']['login'] == user, msgs_sorted)
+        msgs_filtered = filter(
+            lambda x: x['author']['login'] == user, msgs_sorted)
         msgs_filtered = list(msgs_filtered)
 
         msg = msgs_filtered[0]
@@ -103,38 +104,55 @@ class Threads:
 
         return msg, timestamp
 
+    def get_recent_threads(self, limit: int = 20) -> List[dict]:
+        one_day_ago = datetime.now(pytz.utc) - timedelta(days=1)
+        one_day_ago_str = one_day_ago.isoformat()
+
+        threads = self.thread_list(limit=limit)
+        recent_threads = [t for t in threads if t['lastMessage']
+                          ['createdAt'] >= one_day_ago_str]
+        return recent_threads
+
+
 if __name__ == '__main__':
     import os
     import sys
+    from datetime import datetime, timedelta
+    import pytz
 
     sys.path.append(os.getcwd())
-    
+
     from src.auth.OAuth import OAuth
     from dotenv import load_dotenv
 
     load_dotenv()
     oauth = OAuth(
-        client_id = os.environ['CLIENT_ID'],
-        client_secret= os.environ['CLIENT_SECRET']
+        client_id=os.environ['CLIENT_ID'],
+        client_secret=os.environ['CLIENT_SECRET']
     )
     token = oauth.token[0]
 
     threads = Threads(access_token=token)
 
-    threads_list = threads.thread_list
-    thread = threads.get(threads_list[0]['id'])
+    recent_threads = threads.get_recent_threads()
+    print(recent_threads)
 
-    root = "adrianq123"
-    user = "bednarekparts"
+    # threads_list = threads.thread_list
+    # thread = threads.get(threads_list[0]['id'])
 
-    msgs = threads.list_messages(threads_list[0]['id'])
+    # root = "adrianq123"
+    # user = "bednarekparts"
 
-    root_last_msg = threads.get_last_message_from_user(msgs['messages'], root)
-    client_last_msg = threads.get_last_message_from_user(msgs['messages'], user)
-    
-    to_answer = root_last_msg[1] < client_last_msg[1]
+    # msgs = threads.list_messages(threads_list[0]['id'])
 
-    if to_answer:
-        threads.send_message(threads_list[0]['id'], "Hej Mordeczko, jak tam? Nasz pracownik wkrótce się z Tobą skonktuje.")
+    # root_last_msg = threads.get_last_message_from_user(msgs['messages'], root)
+    # client_last_msg = threads.get_last_message_from_user(
+    #     msgs['messages'], user)
+
+    # to_answer = root_last_msg[1] < client_last_msg[1]
+
+    # if to_answer:
+    #     threads.send_message(
+    #         threads_list[0]['id'], "Hej Mordeczko, jak tam? Nasz pracownik wkrótce się z Tobą skonktuje.")
 
     pass
